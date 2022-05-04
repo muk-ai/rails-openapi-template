67434f9f2989533353d274ffbfe59e916ee5c59b
Kenta Mukai <r@muk.ai>
2022-05-04 18:56:32 +0900

    :green_heart: use assert_response_schema_confirm

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
+  include Committee::Rails::Test::Methods
+
+  def committee_options
+    @committee_options ||= {
+      schema_path: Rails.root.join('schema', 'schema.yaml').to_s,
+      query_hash_key: 'rack.request.query_hash',
+      parse_response_by_content_type: false,
+    }
+  end
+
   describe "GET /tasks" do
     it "returns 200" do
       get '/v1/tasks'
-      expect(response).to have_http_status(200)
+      assert_response_schema_confirm(200)
     end
   end
 
   describe "GET /tasks/:id" do
     it "returns 200" do
       get '/v1/tasks/1'
-      expect(response).to have_http_status(200)
+      assert_response_schema_confirm(200)
     end
   end
 end
