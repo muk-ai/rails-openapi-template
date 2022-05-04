b5b413c78b7b135ff813c278f57181b8dcf7bfc4
Kenta Mukai <r@muk.ai>
2022-05-04 18:44:43 +0900

    :green_heart: make it works

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
-    it "works! (now write some real specs)" do
-      get tasks_path
+    it "returns 200" do
+      get '/v1/tasks'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /tasks/:id" do
+    it "returns 200" do
+      get '/v1/tasks/1'
       expect(response).to have_http_status(200)
     end
   end
