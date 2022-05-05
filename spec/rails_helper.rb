93a243c625e7ec551a4591d537372791c7086cf9
Kenta Mukai <r@muk.ai>
2022-05-05 15:20:27 +0900

    :gear: move committee options

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.add_setting :committee_options
+  config.committee_options = {
+    schema_path: Rails.root.join('schema', 'schema.yaml').to_s,
+    query_hash_key: 'rack.request.query_hash',
+    parse_response_by_content_type: false,
+  }
+  config.include Committee::Rails::Test::Methods, type: :request
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
-  include Committee::Rails::Test::Methods
-
-  def committee_options
-    @committee_options ||= {
-      schema_path: Rails.root.join('schema', 'schema.yaml').to_s,
-      query_hash_key: 'rack.request.query_hash',
-      parse_response_by_content_type: false,
-    }
-  end
-
   describe "GET /tasks" do
     it "returns 200" do
       get '/v1/tasks'
