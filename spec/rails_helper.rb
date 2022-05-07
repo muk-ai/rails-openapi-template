51b2323e893e68009790b94685d8af5692abe41c
Kenta Mukai <r@muk.ai>
2022-05-07 22:16:36 +0900

    :gear: add prefix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,7 @@ RSpec.configure do |config|
   config.committee_options = {
     schema_path: Rails.root.join('schema', 'schema.yaml').to_s,
     query_hash_key: 'rack.request.query_hash',
+    prefix: '/v1',
     parse_response_by_content_type: false,
   }
   config.include Committee::Rails::Test::Methods, type: :request
