54f749ec7f5b606609bcedd838886ad5df15c637
Kenta Mukai <r@muk.ai>
2022-05-14 21:03:23 +0900

    :gear: annotate

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :string           not null
+#  completed   :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
